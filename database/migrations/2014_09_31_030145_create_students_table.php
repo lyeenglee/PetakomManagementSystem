51b46652d2f606e55ae13dfe23ebb5a6af499f7b
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2023-01-03 14:39:07 +0800

    Create 2014_09_31_030145_create_students_table.php

---
+++ database/migrations/2014_09_31_030145_create_students_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id('studentID');
+            $table->string('studentName');
+            $table->string('stdaddress');
+            $table->string('studentPhone');
+            $table->string('stdemail');
+            $table->integer('stdyear');
+            $table->string('stdsupervisor');
+            $table->string('stdpsmtitle');
+            $table->string('psmType');
+            $table->string('image');
+            $table->string('industry_status')->default('Unassigned');
+            // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+};
