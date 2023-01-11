d71f2cb7b036b11d73259000bd128c764cdefa5a
Paul0426 <paullaw9904@gmail.com>
2023-01-12 01:36:09 +0800

    proposal table

---
+++ database/migrations/2023_01_09_063346_create_proposal_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proposals', function (Blueprint $table) {
+            $table->id();
+            $table->string('proposalTitle')->nullable();
+            $table->date('date')->nullable();
+            $table->longText('proposalDescription')->nullable();
+            $table->string('proposalUrl')->nullable();
+            $table->string('firstName')->nullable();
+            $table->string('lastName')->nullable();
+            $table->integer('phoneNumber')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proposal');
+    }
+};
