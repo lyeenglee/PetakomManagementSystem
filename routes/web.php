068d0776fecc63d5f5e16b2dacf2c8f017f8356c
Paul0426 <paullaw9904@gmail.com>
2023-01-12 01:59:16 +0800

    add Proposal Controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ActivityController;
+use App\Http\Controllers\ProposalController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 /*
