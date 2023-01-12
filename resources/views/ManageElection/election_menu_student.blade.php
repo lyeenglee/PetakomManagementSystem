de90ca9ea686dd7319c22e635deeaa38e1f23cd9
laggerbomb <prolohzh@gmail.com>
2023-01-12 13:58:22 +0800

    Improve interface menu student
    Improve interface menu student by removing extra space

--- resources/views/ManageElection/election_menu_student.blade.php
+++ resources/views/ManageElection/election_menu_student.blade.php
@@ -17,7 +17,7 @@
                                 <tr>
                                     <td class="col-6" style="text-align: center;"><img src="{{ asset('electionAssets/StudentMenuImage/candidate.png') }}" alt="candidate logo" style="max-width:30%;height:auto;"/></td>
                                     <td class="col-6" style="vertical-align: middle; ">
-                                        <h1 style="font-weight: bold;">View Candidate</h1><br/>
+                                        <h1 style="font-weight: bold;">View Candidate</h1>
                                     </td>
                                 </tr>
                             </tbody>
@@ -35,7 +35,7 @@
                                 <tr>
                                     <td class="col-6" style="text-align: center;"><img src="{{ asset('electionAssets/StudentMenuImage/vote.png') }}" alt="vote logo" style="max-width:30%;height:auto;"/></td>
                                     <td class="col-6"style="vertical-align: middle;">
-                                        <h1 style="font-weight: bold;">Vote Candidate</h1><br/>
+                                        <h1 style="font-weight: bold;">Vote Candidate</h1>
                                     </td>
                                 </tr>
                             </tbody>
@@ -53,7 +53,7 @@
                                 <tr>
                                     <td class="col-6" style="text-align: center;"><img src="{{ asset('electionAssets/StudentMenuImage/committee.png') }}" alt="committee logo" style="max-width:30%;height:auto;"/></td>
                                     <td class="col-6"style="vertical-align: middle;">
-                                        <h1 style="font-weight: bold;">View Committee</h1><br/>
+                                        <h1 style="font-weight: bold;">View Committee</h1>
                                     </td>
                                 </tr>
                             </tbody>
