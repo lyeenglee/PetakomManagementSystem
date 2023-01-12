5748c18b0c3c3b53ec24479771f5a6c7260a8f55
laggerbomb <prolohzh@gmail.com>
2023-01-12 11:12:06 +0800

    Completed Student View Candidate
    Completed Student View Candidate

---
+++ resources/views/ManageElection/student_view_candidate.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.app')
+
+@section('content')
+
+@section('title', 'Student View Candidate')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h3 style="text-align:center;font-weight: bold;">View Candidate</h3><br/>
+            <div class="card">
+                <div class="card-body">
+                    <!-- Display image and name -->
+                    <div class="text-center">
+                        <img src="{{ asset('electionAssets/CandidateImage/'. $electionID->filePath) }}" alt="image of candidate" style="max-width:20%;height:auto;"/>
+                        <h3 class="card-title" style="font-weight: bold;">{{$electionID->name}}</h3><br/><br/>
+                    </div>
+
+                    <div class="table-responsive" style="margin-left: auto; margin-right: auto; width: 70%;">
+                        <table class="table table-borderless">
+                            <tbody>
+                                <!-- Display candidate year -->
+                                <tr>
+                                    <td class="col-5"><label>Year</label></td>
+                                    <td class="col-7"><p style="font-weight: bold;">{{$electionID->year}} </p></td>
+                                </tr>
+
+                                <!-- Display candidate category -->
+                                <tr>
+                                    <td class="col-5"><label>Category</label></td>
+                                    <td class="col-7"><p style="font-weight: bold;">{{$electionID->category}} </p></td>
+                                </tr>
+
+                                <!-- Display candidate course -->
+                                <tr>
+                                    <td class="col-5"><label>Course</label></td>
+                                    <td class="col-7"><p style="font-weight: bold;">{{$electionID->course}} </p></td>
+                                </tr>
+
+                                <!-- Display candidate manifesto -->
+                                <tr>
+                                    <td class="col-5"><label>Manifesto</label></td>
+                                    <td class="col-7"><p style="font-weight: bold; white-space: pre-line;">{{$electionID->manifesto}} </p></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <!-- Back Btn Field -->
+                    <div class="col">
+                        <div class="text-center">
+                            <a href="{{ url('/student/election/studentViewCandidateMenu') }}" class="col-2 btn btn-secondary pull-right">Back</a>
+                        </div>
+                    </div>    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
