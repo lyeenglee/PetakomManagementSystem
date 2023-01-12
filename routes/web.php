af4f2bc95bf377c7166789ddf528435691b06fd8
laggerbomb <prolohzh@gmail.com>
2023-01-12 23:57:23 +0800

    Completed all route for Election
    Completed all route for Election

--- routes/web.php
+++ routes/web.php
@@ -108,9 +108,10 @@ Route::patch('/coordinator/election/edit/{id}', [ElectionController::class, 'coo
 
 //Election- Student
 Route::get('/student/election/studentViewCandidateMenu', [ElectionController::class, 'studentViewCandidateMenu']);
-Route::get('/student/election/studentVoteCandidateMenu', [ElectionController::class, 'studentVoteCandidate']);
-Route::get('/student/election/studentViewCommitteeMenu', [ElectionController::class, 'studentViewCommitteeMenu']);
 Route::get('/student/election/viewCandidate/{id}', [ElectionController::class, 'studentViewCandidate']);
+Route::get('/student/election/studentVoteCandidateMenu', [ElectionController::class, 'studentVoteCandidatePage']);
+Route::patch('/student/election/studentVoteCandidate', [ElectionController::class, 'studentVoteCandidate']);
+Route::get('/student/election/studentViewCommitteeMenu', [ElectionController::class, 'studentViewCommitteeMenu']);
 Route::get('/student/election/viewCommittee/{id}', [ElectionController::class, 'studentViewCommittee']);
 
 //Election- Dean
