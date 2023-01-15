467e01d06e99d4bc443be9187446e81309740fc1
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2023-01-15 22:13:12 +0800

    nav

--- resources/views/ManageRegistration/dean_home.blade.php
+++ resources/views/ManageRegistration/dean_home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.deannav')
 
 @section('content')
 <div class="container">
--- resources/views/ManageRegistration/lecturer_home.blade.php
+++ resources/views/ManageRegistration/lecturer_home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.lecturernav')
 
 @section('content')
 <div class="container">
