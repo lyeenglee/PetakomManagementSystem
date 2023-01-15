6a09ac7241f44986f423d870f20a27f97d858600
lyeenglee <102346135+lyeenglee@users.noreply.github.com>
2023-01-15 21:49:28 +0800

    Update home.blade.php

--- resources/views/ManageRegistration/home.blade.php
+++ resources/views/ManageRegistration/home.blade.php
@@ -1,11 +1,11 @@
-@extends('layouts.app')
+@extends('layouts.studentnav')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Student Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
